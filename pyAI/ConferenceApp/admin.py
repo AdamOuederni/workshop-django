000ad34fdd0483e2cb325854ce85acca5aab8e8c
AdamOuederni <Adam.Ouederni@esprit.tn>
2025-10-10 14:53:33 +0100

    personnalisation Conferenceapp

--- pyAI/ConferenceApp/admin.py
+++ pyAI/ConferenceApp/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
 from .models import Conference,Submission,OrganizingCommittee
 # Register your models here.
-admin.site.register(Conference)
+# admin.site.register(Conference)
 admin.site.register(Submission)
 admin.site.register(OrganizingCommittee)
 admin.site.site_title="Gestion Conférence 25/26"
 admin.site.site_header="Gestion Conférences"
 admin.site.index_title="django app conférence"
+class SubmissionInline(admin.TabularInline):
+    model = Submission
+    extra=1
+    readonly_fields=("submission_date",)
+    
+@admin.register(Conference)
+class AdminConferenceModel(admin.ModelAdmin):
+    list_display=("name","theme","start_date","end_date","duration")
+    ordering=("start_date","end_date")
+    list_filter=("theme",)
+    search_fields=("name",)
+    date_hierarchy="start_date"
+    fieldsets=("information général",{"fields":("conference_id","name","theme","description")}),("logistics Info",{"fields":("location","start_date","end_date")})
+    readonly_fields=("conference_id",)
+    def duration(self,objet):
+        if objet.start_date and objet.end_date:
+            return (objet.end_date - objet.start_date).days
+        return "RAS"
+    duration.short_description="Duration (days)"
+    inlines=[SubmissionInline]
+        
+
--- pyAI/ConferenceApp/models.py
+++ pyAI/ConferenceApp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.core.validators import MaxLengthValidator
 from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+from django.utils.crypto import get_random_string
+from django.utils import timezone
 # Create your models here.
+def validate_keywords(value):
+    mots = [mot.strip() for mot in value.split(',')]
+    if len(mots) > 10:
+        raise ValidationError("Vous ne pouvez pas avoir plus de 10 mots-clés.")
 class Conference(models.Model):
     conference_id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=255)
@@ -20,18 +27,22 @@ class Conference(models.Model):
     end_date=models.DateField()
     created_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"La conférenence a comme titre {self.name}"
     def clean(self):
-        if self.start_date>self.end_date:
-            raise ValidationError("la date de début doit etre inferieure a la date de fin")
+        if self.start_date and self.end_date:
+            if self.start_date>self.end_date:
+                raise ValidationError("la date de début doit etre inferieure a la date de fin")
+            
 
 
 class Submission(models.Model):
     submission_id=models.CharField(max_length=255,primary_key=True,unique=True)
     title=models.CharField(max_length=50)
     abstract=models.TextField()
-    keyword=models.TextField()
+    keyword=models.TextField(validators=[validate_keywords])
     paper=models.FileField(
-        upload_to="papers/"
+        upload_to="papers/",validators=[FileExtensionValidator(['pdf'], "Le fichier doit être un .pdf")]
     )
     STATUS=[
         ("submitted","submitted"),
@@ -46,19 +57,41 @@ class Submission(models.Model):
     update_at=models.DateField(auto_now_add=True)
     user=models.ForeignKey("UserApp.User",on_delete=models.CASCADE,related_name="submissions")
     conference=models.ForeignKey(Conference,on_delete=models.CASCADE,related_name="submissions")
+    def save(self, *args, **kwargs):
+        if not self.submission_id:
+            self.submission_id = "SUB" + get_random_string(8).upper()
+        super().save(*args, **kwargs)
+
+
+    def clean(self):
+        if self.conference.start_date:
+            today = timezone.now().date()
+            if self.conference.start_date <= today:
+                raise ValidationError("La soumission ne peut être faite que pour des conférences à venir.")
+  
+            same_day_submissions = Submission.objects.filter(
+                user=self.user,
+                submission_date=today
+            ).exclude(pk=self.pk).count()
+            if same_day_submissions >= 3:
+                raise ValidationError("Vous ne pouvez pas soumettre à plus de 3 conférences par jour.")
+        
+
+   
 
 class OrganizingCommittee(models.Model):
-    ROLE=[
-        ("chair","chair"),
-        ("co-chair","co-chair"),
-        ("member","member"),
+    chair = [
+        ('chair','chair'),
+        ('co-chair','co-chair'),
+        ('member','member')
     ]
-    committee_role=models.CharField(max_length=255,choices=ROLE,default="member")
-    date_joined=models.DateField()
-    created_at=models.DateField(auto_now_add=True)
-    update_at=models.DateField(auto_now_add=True)
-    user=models.ForeignKey("UserApp.User",on_delete=models.CASCADE,related_name="committees")
-    conference=models.ForeignKey(Conference,on_delete=models.CASCADE,related_name="committees")
+    commitee_role = models.CharField(max_length=255,choices=chair)
+    join_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # user = models.ForeignKey("UserApp.User",on_delete=models.CASCADE,related_name='committees')
+    user = models.ForeignKey("UserApp.USER", on_delete=models.CASCADE, related_name='committees')
+    conference = models.ForeignKey('ConferenceApp.CONFERENCE',on_delete=models.CASCADE,related_name='committees')
 
 
 
