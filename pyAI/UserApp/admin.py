95a789f09ab4f466db974bea7fa395565701c898
AdamOuederni <Adam.Ouederni@esprit.tn>
2025-10-11 17:53:05 +0100

    personnalisation admin user

--- pyAI/UserApp/admin.py
+++ pyAI/UserApp/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from .models import User
 # Register your models here.
-admin.site.register(User)
+#admin.site.register(User)
+@admin.register(User)
+class AdminUserModel(admin.ModelAdmin):
+    list_display=("full_name","email","role","nationality","created_at")
+    ordering=("last_name",)
+    list_filter=("role",)
+    search_fields=("first_name","last_name")
+    #fieldsets=("informations générales",{"fields":("first_name","last_name","role","affiliation","nationality")}),("Contact Info",{"fields":("email",)})
+    fieldsets = (
+        ("Informations générales", {
+            "fields": ("first_name", "last_name", "role", "affiliation", "nationality")
+        }),
+        ("Contact Info", {
+            "fields": ("email",)
+        }),
+    )
+    def full_name(self, obj):
+        if obj.first_name and obj.last_name:
+            return f"{obj.first_name} {obj.last_name}"
+        return "RAS"
+    full_name.short_description = "Nom complet"
+    date_hierarchy = "created_at"
+    readonly_fields=("user_id",)
+    list_display_links = ("email","full_name")
+
